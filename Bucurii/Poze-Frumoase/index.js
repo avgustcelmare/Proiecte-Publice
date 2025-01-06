cddfa60f281f29362c2b498e7f2c7a72647bb35f
avgustcelmare <avgustcelmare@yahoo.com>
2025-01-07 01:59:02 +0200

    Pagina cu Poze Frumoase

---
+++ Bucurii/Poze-Frumoase/index.js
@@ -0,0 +1,26 @@
+const imageContainerEl = document.querySelector(".image-container");
+
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("click", () => {
+  imaginiPerClick = 3;
+  addNewImages();
+  scrollToButton();
+});
+
+function addNewImages () {
+  for (let index = 0; index < imaginiPerClick; index++) {
+    const newImageEl = document.createElement("img");
+
+    newImageEl.src = `https://picsum.photos/300?random=${Math.floor(Math.random()*20000)}`;
+  
+    imageContainerEl.appendChild(newImageEl);
+  }
+};
+
+function scrollToButton() {
+  btnEl.scrollIntoView({
+    behavior: "smooth", 
+    block: "center"  
+  });
+}
