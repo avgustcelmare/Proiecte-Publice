41729fb55464d2999ddc0194c2ed34700fcb362a
avgustcelmare <avgustcelmare@yahoo.com>
2025-02-03 22:53:00 +0200

    Pagina Verifică Mărime Ecran

---
+++ Utile/Verifica-Marime-Ecran/index.js
@@ -0,0 +1,7 @@
+const inaltimeaEl = document.querySelector(".inaltimea");
+
+const latimeaEl = document.querySelector(".latimea");
+
+inaltimeaEl.innerText = window.screen.height;
+
+latimeaEl.innerText = window.screen.width;
